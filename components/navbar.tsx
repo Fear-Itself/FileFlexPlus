c2fcaa47380591d51a2beceed7d2332f026e68be
Fear <ryan.m.lippman@gmail.com>
2024-11-17 13:28:20 -0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,13 +18,13 @@ export default function Navbar({}): any {
       </Link>
       <div className="items-center hidden gap-2 md:flex">
         <ModeToggle />
-        <Link href="https://github.com/thegr8binil/FileFlex.git">
+        <Link href="https://github.com/fear-itself">
           <Button
             variant="default"
             className="items-center hidden gap-2 rounded-full bg-primary w-fit md:flex"
             size="lg"
           >
-            <span>Github Repo</span>
+            <span>My Github</span>
             <span className="text-xl">
               <BsGithub />
             </span>
