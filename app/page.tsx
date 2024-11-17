ca54e5669a8d2be7ef018ba919a9f992b902ab9d
Fear <ryan.m.lippman@gmail.com>
2024-11-17 13:32:34 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
     
       <div className="space-y-6">
         <h1 className="text-3xl font-medium text-center md:text-5xl">
-          Free Unlimited File Converter with FileFlex
+          FileFlexPlus
         </h1>
         <p className="text-center text-muted-foreground text-md md:text-lg md:px-24 xl:px-44 2xl:px-52">
-          Introducing FileFlex – your go-to online tool for unlimited and free
-          multimedia conversion. Easily convert images, audio, and videos
-          without any restrictions. Start converting now and streamline your
-          content effortlessly with FileFlex!
+          Introducing FileFlexPlus – your ultimate online solution for unlimited and free multimedia conversion. 
+          Effortlessly convert images, audio, and videos with no restrictions. 
+          Whether you’re working with media for personal or professional use, FileFlexPlus streamlines the process so you can focus on what matters most. 
+          Start converting now and experience seamless, high-quality content transformation with ease!
         </p>
       </div>
       <Dropzone />
